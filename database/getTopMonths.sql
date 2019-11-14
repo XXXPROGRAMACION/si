7a25267a794e942e8008b01629a3aebb8fdc246c
Alejandro Pascual Pozo <alejandropascualpozo@gmail.com>
2019-11-14 20:09:28 +0100

    Añadido primer trigger

--- database/getTopMonths.sql
+++ database/getTopMonths.sql
@@ -3,15 +3,14 @@ SELECT EXTRACT(year FROM orderdate) AS year, EXTRACT(month FROM orderdate) AS mo
 FROM orders GROUP BY year, month ORDER BY year, month;
 
 CREATE OR REPLACE VIEW months_products AS
-SELECT EXTRACT(year FROM orderdate) AS year, EXTRACT(month FROM orderdate) AS month, COUNT(*) AS products
+SELECT EXTRACT(year FROM orderdate) AS year, EXTRACT(month FROM orderdate) AS month, SUM(quantity) AS products
 FROM orders AS o JOIN orderdetail AS od ON o.orderid=od.orderid GROUP BY year, month ORDER BY year, month;
 
-CREATE OR REPLACE FUNCTION getTopVentas(min_totalamount int, min_products int)
-RETURNS TABLE (year double precision, month double precision, totalamount numeric, products bigint) AS $$ BEGIN
+CREATE OR REPLACE FUNCTION getTopVentas(min_totalamount int, min_products int) RETURNS TABLE (year double precision, month double precision, totalamount numeric, products bigint) AS $$ BEGIN
 	RETURN QUERY
 		SELECT mta.year, mta.month, mta.totalamount, mp.products
 		FROM months_totalamount AS mta JOIN months_products AS mp ON mta.year=mp.year AND mta.month=mp.month
 		WHERE mta.totalamount>=min_totalamount OR mp.products>=min_products ORDER BY mta.year, mta.month;
 END $$ LANGUAGE plpgsql;
 
---SELECT * FROM getTopVentas(300000, 17000);
+SELECT * FROM getTopVentas(320000, 19000);
--- database/setOrderAmount.sql
+++ database/setOrderAmount.sql
@@ -1,9 +1,8 @@
 UPDATE orders SET netamount=NULL, totalamount=NULL;
-CREATE OR REPLACE VIEW orderdetail_netamount AS SELECT orderid, SUM(price) AS netamount FROM orderdetail GROUP BY orderid;
-CREATE OR REPLACE PROCEDURE setOrderAmount() LANGUAGE SQL AS
-$$
-    UPDATE orders AS o SET netamount=odn.netamount, totalamount=odn.netamount*(100+o.tax)/100
+CREATE OR REPLACE VIEW orderdetail_netamount AS SELECT orderid, SUM(price*quantity) AS netamount FROM orderdetail GROUP BY orderid;
+CREATE OR REPLACE FUNCTION setOrderAmount() RETURNS void AS $$
+    UPDATE orders AS o SET netamount=odn.netamount, totalamount=ROUND(CAST(odn.netamount*(100+o.tax)/100 AS numeric), 2)
 	FROM orderdetail_netamount AS odn WHERE o.netamount IS NULL AND o.orderid=odn.orderid;
-$$;
-CALL setOrderAmount();
-SELECT * FROM orders;
+$$ LANGUAGE sql;
+-- SELECT setOrderAmount();
+-- SELECT * FROM orders;
